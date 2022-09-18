05816935aedb78180962f41706cd52ba9f7eb4b0
UEFI <1092700291@qq.com>
2022-09-18 06:25:32 +0000

    Update

--- co2.py
+++ co2.py
@@ -32,6 +32,9 @@ def checkBalance(address):
 
 def verifyTransaction(transaction):
     sender = transaction[0:192]
+    if sender == "0" * 192:
+        # Mining reward, only check PoW
+        return True
     receiver = transaction[192:384]
     amount = transaction[384:448] # 64 bytes
     asset = transaction[448:768]  # 320 bytes
@@ -46,7 +49,7 @@ def verifyPoW(block):
 
 def verifyBlock(block):
     if len(chain) == 0:
-        priousHash = "0" * 64
+        previousHash = "0" * 64
     else:
         previousHash = chain[-1][1536:1600]
     blockPrivousHash = block[0:64]
@@ -62,28 +65,52 @@ def verifyBlock(block):
     return verifyPoW(blockPoW)
 
 def makeTransaction(sender, receiver, amount, asset, sk):
-    if amount <= 0:
-        print("Amount must be positive!")
-        return False
-    if checkBalance(sender) < amount:
-        print("Not enough balance!")
-        return False
-    transaction = sender + receiver + str(amount).zfill(64) + asset
-    sig = sign(sha256(transaction), sk)
-    return transaction + sig
+    if sender == "0" * 192:
+        # Mining reward
+        transaction = sender + receiver + str(amount).zfill(64) + asset
+        sig = "0" * 192
+        return transaction + sig
+    else:
+        if amount <= 0:
+            print("Amount must be positive!")
+            return False
+        if checkBalance(sender) < amount:
+            print("Not enough balance!")
+            return False
+        transaction = sender + receiver + str(amount).zfill(64) + asset
+        sig = sign(sha256(transaction), sk)
+        return transaction + sig
 
 def makeBlock(transaction, pow):
     if len(chain) == 0:
-        priousHash = "0" * 64
+        previousHash = "0" * 64
     else:
         previousHash = chain[-1][1536:1600]
     block = previousHash + transaction + pow
     return block + sha256(block)
+
+def makeMiningReward(receiver):
+    reward = makeTransaction("0" * 192, receiver, 100, "0" * 320, "0" * 64)
+    mined = makeBlock(reward, "0" * 512)
+    print("Mining block: " + mined)
+    print("Block is valid: " + str(verifyBlock(mined)))
+    if verifyBlock(mined):
+        chain.append(mined)
 
 if __name__ == '__main__':
     sk1 = '9061086a4cb981d215f9875028b6217e812df3a6452abb6df9a23a014a9c40694cb6be00efb817a2ca22393281df8a40'
     vk1 = '75d6c47c5287e9b81fb8472d9ebf6635b5e20f5156d4bacb395997767790120b21fbd9ca67d7eed13ff68e3d8de25bd41ffa661742dcd41a00d83f02bcffd0205f248ea2dcb9432454de53097c78f79e6e0f50f5831ddb8ef4e57e7d1288e82e'
     sk2 = 'd10e1416590b938fc8e57088071af877d81ff20dc39707caa3866894a2a80eb59f249184a6e7f2ac25148082aeadbb24'
     vk2 = '6d731960af08e06ba3e84f660af4af9ccde5d47ba9070602a27687ba6e39b5cb778adcbca6fdef19e99edef115ec6fc9711867ae7e185922596c246f0734640d857c2d7e3f4d87aca6b02f7a891bdccd50ab712af753f0c7d987534bd94fab2b'
-    
+    makeMiningReward(vk1)
+    print("Balance of vk1: " + str(checkBalance(vk1)))
+    trans = makeTransaction(vk1, vk2, 12, '0' * 320, sk1)
+    if trans != False:
 
+        block = makeBlock(trans, '0' * 512)
+        print("Block is valid: " + str(verifyBlock(block)))
+        if verifyBlock(block):
+            chain.append(block)
+    print("Balance of vk1: " + str(checkBalance(vk1)))
+    print("Balance of vk2: " + str(checkBalance(vk2)))
+    print(chain)
