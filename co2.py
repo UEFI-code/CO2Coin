2d7fad72d3fc65ecda7c99352e4c9882ac54f535
UEFI <1092700291@qq.com>
2022-09-18 11:22:45 +0000

    Bug Fix

--- co2.py
+++ co2.py
@@ -28,10 +28,13 @@ def checkBalance(address):
         sender = blockData[0:192]
         receiver = blockData[192:384]
         amount = blockData[384:448]
+        miner = block[1024:1216]
         if sender == address:
             balance -= (int(amount) + fee)
         if receiver == address:
             balance += int(amount)
+        if miner == address:
+            balance += rewardcoins
     return balance
 
 def checkDoubleSpend(transaction):
@@ -46,8 +49,11 @@ def verifyTransaction(transaction):
         return False
     sender = transaction[0:192]
     if sender == "0" * 192:
-        # Mining reward, check PoW
-        return True
+        if(len(jobs) == 0):
+            print("No jobs to verify, but will give you mined reward!")
+            return True
+        print('Pls verify blocks, dont do white!')
+        return False
     receiver = transaction[192:384]
     amount = transaction[384:448] # 64 bytes
     asset = transaction[448:768]  # 320 bytes
@@ -61,22 +67,24 @@ def verifyPoW(PoW):
     return True
 
 def verifyBlock(block):
-    if(len(block) != 1600):
+    if(len(block) != 1792):
         # Data length is wrong
         return False
     if len(chain) == 0:
         previousHash = "0" * 64
     else:
-        previousHash = chain[-1][1536:1600]
+        previousHash = chain[-1][1728:1792]
     blockPrivousHash = block[0:64]
     if(blockPrivousHash != previousHash):
         return False
     blockData = block[64:1024]
     if not verifyTransaction(blockData):
         return False
-    blockPoW = block[1024:1536]
-    blockHash = block[1536:1600]
-    if(blockHash != sha256(blockPrivousHash + blockData + blockPoW)):
+    blockMiner = block[1024:1216] # 192 bytes
+    blockPoW = block[1216:1728] # 512 bytes
+    blockHash = block[1728:1792] # 64 bytes
+
+    if(blockHash != sha256(blockPrivousHash + blockData + blockMiner + blockPoW)):
         return False
     return verifyPoW(blockPoW)
 
@@ -92,21 +100,20 @@ def makeTransaction(sender, receiver, amount, asset, sk):
     sig = sign(sha256(transaction), sk)
     return transaction + sig
 
-def makeBlock(transaction, pow):
+def makeBlock(transaction, miner, pow):
     if len(chain) == 0:
         previousHash = "0" * 64
     else:
-        previousHash = chain[-1][1536:1600]
-    block = previousHash + transaction + pow
+        previousHash = chain[-1][1728:1792]
+    block = previousHash + transaction + miner + pow
     return block + sha256(block)
 
-def makeMiningReward(receiver, pow):
-    rewardTrans = makeTransaction("0" * 192, receiver, rewardcoins, "!" * 320, "6" * 96)
-    if rewardTrans != False:
-        minedBlock = makeBlock(rewardTrans, pow)
-        if verifyBlock(minedBlock):
-            chain.append(minedBlock)
-            return True
+def makeWhiteMiningReward(receiver, pow):
+    rewardTrans = makeTransaction("0" * 192, "0" * 192, 0, "!" * 320, SigningKey.generate(curve=NIST384p).to_string().hex())
+    minedBlock = makeBlock(rewardTrans, receiver, pow)
+    if verifyBlock(minedBlock):
+        chain.append(minedBlock)
+        return True
     return False
 
 def demo():
@@ -114,18 +121,27 @@ def demo():
     vk1 = '75d6c47c5287e9b81fb8472d9ebf6635b5e20f5156d4bacb395997767790120b21fbd9ca67d7eed13ff68e3d8de25bd41ffa661742dcd41a00d83f02bcffd0205f248ea2dcb9432454de53097c78f79e6e0f50f5831ddb8ef4e57e7d1288e82e'
     sk2 = 'd10e1416590b938fc8e57088071af877d81ff20dc39707caa3866894a2a80eb59f249184a6e7f2ac25148082aeadbb24'
     vk2 = '6d731960af08e06ba3e84f660af4af9ccde5d47ba9070602a27687ba6e39b5cb778adcbca6fdef19e99edef115ec6fc9711867ae7e185922596c246f0734640d857c2d7e3f4d87aca6b02f7a891bdccd50ab712af753f0c7d987534bd94fab2b'
-    makeMiningReward(vk1, 'p' * 512)
-    makeMiningReward(vk1, 'p' * 512)
+    res = makeWhiteMiningReward(vk1, 'p' * 512)
+    if res:
+        print("White mining success!")
+    else:
+        print("White mining failed!")
+    res = makeWhiteMiningReward(vk1, 'p' * 512)
+    if res:
+        print("White mining success!")
+    else:
+        print("White mining failed!")
+
     print("Balance of vk1: " + str(checkBalance(vk1)))
     trans = makeTransaction(vk1, vk2, 12, '0' * 320, sk1)
     if trans != False:
-        block = makeBlock(trans, 'p' * 512)
+        block = makeBlock(trans, vk2, 'p' * 512)
         if verifyBlock(block):
             print("Block is valid")
             chain.append(block)
     trans = makeTransaction(vk1, vk2, 12, '0' * 320, sk1)
     if trans != False:
-        block = makeBlock(trans, 'p' * 512)
+        block = makeBlock(trans, vk2, 'p' * 512)
         if verifyBlock(block):
             print("Block is valid")
             chain.append(block)
--- co2srv.py
+++ co2srv.py
@@ -1,17 +1,21 @@
+from site import removeduppaths
 import socket
 import co2
-
+from threading import Thread
+import os
 nodelist = []
 
 def listener():
-    s = socket.socket()
     host = socket.gethostname()
     port = 2333
-    s.bind((host, port))
-    s.listen()
     while True:
+        s = socket.socket()
+        s.bind((host, port))
+        s.listen()
         c, addr = s.accept()
-        nodelist.append(addr)
+        c.settimeout(3.0)
+        if addr[0] != host and addr[0] not in nodelist:
+            nodelist.append(addr[0])
         c.send(b'CO2Srv')
         buf = c.recv(32)
         if buf.decode() == 'getblockheight':
@@ -26,15 +30,18 @@ def listener():
         elif buf.decode() == 'verifyblock':
             c.send(b'ready')
             block = c.recv(1600).decode()
-            if block not in co2.chain and co2.verifyBlock(block):
-                co2.chain.append(block)
-                for i in range(len(co2.jobs)):
-                    if co2.jobs[i] == block[64:1024]:
-                        co2.jobs.pop(i)
-                        break
-                c.send(b'Verified!')
+            if block not in co2.chain:
+                if co2.verifyBlock(block):
+                    co2.chain.append(block)
+                    for i in range(len(co2.jobs)):
+                        if co2.jobs[i] == block[64:1024]:
+                            co2.jobs.pop(i)
+                            break
+                    c.send(b'Verified!')
+                else:
+                    c.send(b'Verification failed!')
             else:
-                c.send(b'Error block!')    
+                c.send(b'Old block!')  
         elif buf.decode() == 'getbalance':
             c.send(b'ready')
             address = c.recv(192).decode()
@@ -47,6 +54,8 @@ def listener():
                 c.send(b'Verified!')
             else:
                 c.send(b'Error transaction!')
+        elif buf.decode() == 'getnodes':
+            c.send(str(nodelist).encode())
         s.close()    
 
 def boardcastJobs():
@@ -75,3 +84,67 @@ def boardcastBlock():
             if buf.decode() == 'Verified!':
                 print('block sent to %s' %node)
         s.close()
+
+def syncHeight():
+    for node in nodelist:
+        s = socket.socket()
+        s.settimeout(3.0)
+        s.connect((node, 2333))
+        if(s.recv(32).decode() == 'CO2Srv'):
+            s.send(b'getblockheight')
+            s.close()
+            remoteHeight = s.recv(32).decode()
+            print('Remote height: %s' %remoteHeight)
+            if int(remoteHeight) > len(co2.chain):
+                for i in range(len(co2.chain), int(remoteHeight)):
+                    s = socket.socket()
+                    s.settimeout(3.0)
+                    s.connect((node, 2333))
+                    s.send(b'syncblock')
+                    buf = s.recv(32)
+                    if buf.decode() == 'n?':
+                        s.send(str(i).encode())
+                        buf = s.recv(1600).decode()
+                        s.close()
+                        if buf != 'Error id!':
+                            if(co2.verifyBlock(buf)):
+                                co2.chain.append(buf)
+                                print('Block %d synced from %s' %(i, node))
+                            else:
+                                print('Block %d verification failed!' %i)
+                                break
+                        else:
+                            print('Remote Error: Block %d not found!' %i)
+                            break
+
+def updateNodes():
+    host = socket.gethostname()
+    for node in nodelist:
+        s = socket.socket()
+        s.settimeout(3.0)
+        s.connect((node, 2333))
+        if(s.recv(32).decode() == 'CO2Srv'):
+            s.send(b'getnodes')
+            remoteNodes = eval(s.recv(1024).decode())
+            s.close()
+            for node in remoteNodes:
+                if node != host and node not in nodelist:
+                    nodelist.append(node)
+                    print('Node %s added!' %node)
+
+if __name__ == '__main__':
+    print('CO2Srv started.')
+    if(os.path.exists('chain.txt')):
+        co2.chain = open('chain.txt', 'r').readlines()
+    if(os.path.exists('nodelist.txt')):
+        nodelist = open('nodelist.txt', 'r').readlines()
+    updateNodes()
+    syncHeight()
+    net = Thread(target=listener)
+    net.start()
+    while True:
+        if len(co2.jobs) != 0:
+            myjob = co2.jobs[-1]
+            co2.makeBlock(myjob)
+
+  
